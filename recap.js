dfb960769d491c6ce8a8d8e7fa37b55bad856916
dev-rahim <rahimuddin5915@gmail.com>
2022-09-08 08:54:09 -0700

    first commit

---
+++ recap.js
@@ -0,0 +1,11 @@
+//  1. let const 
+const myMotherName = 'Rahima Begum';
+let myAge = 22;
+// 2. tamplate string 
+console.log(`I'm Rahim. I'm ${myAge} years old.`);
+// 3. Array spred 
+const numbers = [12, 33, 44, 55, 4, 3, 5, 4, 65];
+console.log(Math.max(...numbers));
+// 4. Arrow function 
+const add = (a, b) => a + b;
+console.log(add(2, 5));

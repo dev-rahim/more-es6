2110221a88e6766ce16c647ec0d2b822c0ec6166
dev-rahim <rahimuddin5915@gmail.com>
2022-09-08 23:56:21 +0600

    exploring array map

---
+++ map.js
@@ -0,0 +1,13 @@
+const numbers = [10, 52, 6, 2, 32, 62, 32, 52];
+const doubleIt = z => z * 3;
+const output = [];
+// for (const number of numbers) {
+//     // const result = number * 2;
+//     // const result = doubleIt(number);
+//     // output.push(result);
+// }
+const result = numbers.map(x => x * 7);
+
+// console.log(output);
+console.log(result);
+
